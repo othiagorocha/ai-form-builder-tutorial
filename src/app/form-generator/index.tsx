35a77d1e7db35887878c29c485c3236d4195c8d9
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-06-13 15:47:02 -0300

    wip

--- src/app/form-generator/index.tsx
+++ src/app/form-generator/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -10,30 +10,65 @@ import {
   DialogFooter,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { generateForm } from "@/actions/generateForm";
+import { useFormState, useFormStatus } from "react-dom";
 
 type Props = {};
 
+const initialState: {
+  message: string;
+  data?: any;
+} = {
+  message: "",
+};
+
+export function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Generating..." : "Generate"}
+    </Button>
+  );
+}
+
 const FormGenerator = (props: Props) => {
+  const [state, formAction] = useFormState(generateForm, initialState);
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (state.message === "success") {
+      setOpen(false);
+    }
+    console.log(state.data);
+  }, [state]);
 
   const onFormCreate = () => {
     setOpen(true);
   };
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <Button onClick={onFormCreate}>Create form</Button>
       <DialogContent className="sm:max-w-[425px] ">
         <DialogHeader>
           <DialogTitle>Create New Form</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
         </DialogHeader>
-        <DialogFooter>
-          <Button variant="link">Create manually</Button>
-        </DialogFooter>
+        <form action={formAction}>
+          <div className="grid gap-4 py-4">
+            <Textarea
+              id="description"
+              name="description"
+              required
+              placeholder="Share what your form is about, who is it for, and what information you would like to collect. And AI will do the magic"
+            ></Textarea>
+          </div>
+          <DialogFooter>
+            <SubmitButton />
+            <Button variant="link">Create manually</Button>
+          </DialogFooter>{" "}
+        </form>
       </DialogContent>
     </Dialog>
   );
